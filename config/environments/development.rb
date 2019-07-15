d7e18bbdbbd50dfc7dd35e09bc5f839c8b75b78d
toltan <@ez>
2019-07-15 05:45:25 +0000

    refactoring

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'ap-northeast-1.console.aws.amazon.com'
+  host = 'fe0a308087a8413b93eb356b5615d224.vfs.cloud9.ap-northeast-1.amazonaws.com/'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
